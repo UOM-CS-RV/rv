7eb03a30f42558cfe9234e7e78f8cfe5eb55c1f1
Edward Mallia <edmallia@gmail.com>
2016-01-25 21:19:01 +0100

    Adds equals and hashCode methods to UserEvent

--- sample/src/main/java/mt/edu/um/cs/rv/events/UserEvent.java
+++ sample/src/main/java/mt/edu/um/cs/rv/events/UserEvent.java
@@ -1,7 +1,5 @@
 package mt.edu.um.cs.rv.events;
 
-import mt.edu.um.cs.rv.events.Event;
-
 /**
  * Created by dwardu on 20/01/2016.
  */
@@ -22,5 +20,24 @@ public abstract class UserEvent implements Event {
     @Override
     public boolean isSynchronous() {
         return synchronous;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEvent userEvent = (UserEvent) o;
+
+        if (synchronous != userEvent.synchronous) return false;
+        return username != null ? username.equals(userEvent.username) : userEvent.username == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (synchronous ? 1 : 0);
+        return result;
     }
 }
