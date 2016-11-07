5882743b0dcb2c1843dd09ef9e2b9cbb86063530
Edward Mallia <edward.mallia@nemeabank.com>
2016-11-08 00:26:41 +0100

    Makes DirectInvocationEventObserver accessible as a singleton outside the spring context

--- event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/observers/DirectInvocationEventObserver.java
+++ event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/observers/DirectInvocationEventObserver.java
@@ -2,13 +2,19 @@ package mt.edu.um.cs.rv.eventmanager.observers;
 
 import mt.edu.um.cs.rv.events.Event;
 import mt.edu.um.cs.rv.eventmanager.engine.EventMessageSender;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Created by dwardu on 19/01/2016.
  */
-public class DirectInvocationEventObserver implements EventObserver {
+public class DirectInvocationEventObserver implements EventObserver, ApplicationContextAware
+{
 
     EventMessageSender eventMessageSender;
+
+    private static DirectInvocationEventObserver directInvocationEventObserver;
 
     public DirectInvocationEventObserver(EventMessageSender eventMessageSender) {
         this.eventMessageSender = eventMessageSender;
@@ -17,5 +23,22 @@ public class DirectInvocationEventObserver implements EventObserver {
     @Override
     public void observeEvent(Event e) {
         eventMessageSender.send(e);
+    }
+
+    /**
+     * Provides a way to access the spring singleton in a static manner.
+     * @return The spring singleton bean for this class
+     */
+    public static DirectInvocationEventObserver getInstance() {
+        if (DirectInvocationEventObserver.directInvocationEventObserver == null){
+            throw new IllegalStateException("DirectInvocationEventObserver has not been initialised yet.");
+        }
+        return DirectInvocationEventObserver.directInvocationEventObserver;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        DirectInvocationEventObserver.directInvocationEventObserver = this;
     }
 }
