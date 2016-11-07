c4b9752d89f82fd4a1f6fdbdf62890f72d433968
Edward Mallia <edward.mallia@nemeabank.com>
2016-11-08 00:25:36 +0100

    Fixes CategorisedEvent

--- events/src/main/java/mt/edu/um/cs/rv/events/CategorisedEvent.java
+++ events/src/main/java/mt/edu/um/cs/rv/events/CategorisedEvent.java
@@ -3,6 +3,7 @@ package mt.edu.um.cs.rv.events;
 /**
  * Created by dwardu on 18/01/2016.
  */
-public interface Event {
-    boolean isSynchronous();
+public interface CategorisedEvent<C> extends Event
+{
+    C categoriseEvent();
 }
