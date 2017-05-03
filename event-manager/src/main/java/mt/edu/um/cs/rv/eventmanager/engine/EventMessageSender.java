689f33a78f0136f4fb9036edb542a4484f3e86b0
Edward Mallia <edmallia@gmail.com>
2017-05-03 23:18:20 +0200

    Adds logging

--- event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/engine/EventMessageSender.java
+++ event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/engine/EventMessageSender.java
@@ -4,6 +4,8 @@ import mt.edu.um.cs.rv.events.Event;
 import mt.edu.um.cs.rv.monitors.Monitor;
 import mt.edu.um.cs.rv.monitors.results.MonitorResult;
 import mt.edu.um.cs.rv.monitors.results.MonitorResultList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.integration.channel.ExecutorChannel;
 import org.springframework.integration.channel.NullChannel;
 import org.springframework.integration.core.MessagingTemplate;
@@ -18,6 +20,8 @@ import java.util.Collection;
  */
 public class EventMessageSender {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(EventMessageSender.class);
+
     private MessagingTemplate messagingTemplate;
     private ExecutorChannel executorChannel;
     private NullChannel noop;
@@ -29,32 +33,45 @@ public class EventMessageSender {
     }
 
     public MonitorResult send(Event e) {
+
+        LOGGER.debug("Building event system message from event");
         MessageBuilder<Event> eventMessageBuilder = MessageBuilder.withPayload(e);
 
         if (e.isSynchronous()) {
+            LOGGER.debug("Handling event in a synchronous manner ...");
             eventMessageBuilder.setReplyChannelName("systemBlockingChannel");
             Message<Event> eventMessage = eventMessageBuilder.build();
+
+            LOGGER.debug("Sending event system message synchronously");
             Message<?> r = messagingTemplate.sendAndReceive(executorChannel, eventMessage);
 
+            LOGGER.debug("Preparing response");
             Object payload = r.getPayload();
             if (payload instanceof MonitorResult) {
+                LOGGER.debug("Received MonitorResult as response, returning the results");
                 return (MonitorResult) payload;
             }
             else if (payload instanceof Collection) {
+                LOGGER.debug("Received a collection of MonitorResult as response, returning the results as a MonitorResultList");
                 Collection payloadColl = (Collection) payload;      
                 return MonitorResultList.of(new ArrayList<MonitorResult>(payloadColl));
             }
             else {
                 //TODO what to do here?
-                return MonitorResult.ok();
+                LOGGER.debug("Received an unexpected type of response {}. ", payload);
+                return MonitorResult.error();
             }
         }
         else {
+            LOGGER.debug("Handling event in a asynchronous manner ...");
             eventMessageBuilder.setReplyChannel(noop);
             Message<Event> eventMessage = eventMessageBuilder.build();
+
+            LOGGER.debug("Sending event system message asynchronously");
             messagingTemplate.send(executorChannel, eventMessage);
 
             //TODO what to do here?
+            LOGGER.debug("Returning OK for an event system message handled asynchronously.");
             return MonitorResult.ok();
         }
 
--- event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/observers/ExternalEventObserver.java
+++ event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/observers/ExternalEventObserver.java
@@ -5,6 +5,8 @@ import mt.edu.um.cs.rv.events.builders.EventBuilder;
 import mt.edu.um.cs.rv.events.builders.EventBuilderRegistry;
 import mt.edu.um.cs.rv.events.triggers.TriggerData;
 import mt.edu.um.cs.rv.monitors.results.MonitorResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -14,24 +16,35 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public abstract class ExternalEventObserver<M,T extends TriggerData,R>
 {
+    private static Logger LOGGER = LoggerFactory.getLogger(ExternalEventObserver.class);
 
     @Autowired
     EventBuilderRegistry eventBuilderRegistry;
 
     public final void onMessage(M message) {
+        LOGGER.debug("Generating Trigger from Message");
         T trigger = generateTrigger(message);
 
+        LOGGER.debug("Computing whether event should be a/synchronous");
         Boolean shouldEventBeSynchronous = shouldEventBeSynchronous(trigger);
+        LOGGER.debug("Event should be {}", shouldEventBeSynchronous ? "synchronous" : "asynchronous");
 
+        LOGGER.debug("Building event ...");
         EventBuilder builder = eventBuilderRegistry.getBuilder(trigger.getClass());
         Event event = builder.build(trigger, shouldEventBeSynchronous);
+        LOGGER.debug("Built new event {}", event);
         //TODO should this be the default ?
         MonitorResult monitorResult = MonitorResult.ok();
+        LOGGER.debug("Checking whether event should be fired ...");
         if (builder.shouldFireEvent(event)) {
+            LOGGER.debug("Event should be fired. Firing event ...");
             monitorResult = fireEvent(event);
+            LOGGER.debug("Event fired with monitor result {}", monitorResult);
         }
 
+        LOGGER.debug("Generating response for trigger");
         R r = generateResponse(message, trigger, monitorResult);
+        LOGGER.debug("Sending response for trigger");
         sendResponse(r);
     }
 
