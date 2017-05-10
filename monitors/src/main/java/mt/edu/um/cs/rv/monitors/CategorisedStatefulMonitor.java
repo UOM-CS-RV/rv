0fd19a4bf8983708aac965cfeda907e6965334a4
Edward Mallia <edmallia@gmail.com>
2017-05-10 12:14:29 +0200

    Fixes CategorisedStatefulMonitor as the previous signatures were not overriding the base methods.

--- monitors/src/main/java/mt/edu/um/cs/rv/monitors/CategorisedStatefulMonitor.java
+++ monitors/src/main/java/mt/edu/um/cs/rv/monitors/CategorisedStatefulMonitor.java
@@ -1,15 +1,29 @@
 package mt.edu.um.cs.rv.monitors;
 
 import mt.edu.um.cs.rv.events.CategorisedEvent;
+import mt.edu.um.cs.rv.events.Event;
 import mt.edu.um.cs.rv.monitors.state.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by dwardu on 29/04/2017.
  */
 public abstract class CategorisedStatefulMonitor<S extends State> extends StatefulMonitor<S> {
 
-    protected S loadOrCreateState(Class<? extends mt.edu.um.cs.rv.monitors.Monitor> c, CategorisedEvent e, State parentState){
-        State state = this.getMonitorPersistenceProvider().load(c.getTypeName(), e.categoriseEvent());
+    private static Logger LOGGER = LoggerFactory.getLogger(CategorisedStatefulMonitor.class);
+
+    @Override
+    protected S loadOrCreateState(Class<? extends mt.edu.um.cs.rv.monitors.Monitor> c, Event e, State parentState){
+        CategorisedEvent ce = null;
+        if (e instanceof CategorisedEvent){
+            ce = (CategorisedEvent) e;
+        }
+        else{
+            LOGGER.error("Unable to load state for monitor {}, as supplied event [{}] is not an instance of CategorisedEvent", c.getName(), e);
+        }
+
+        State state = this.getMonitorPersistenceProvider().load(c.getTypeName(), ce.categoriseEvent());
 
         if (state == null){
             //create new state
@@ -21,7 +35,17 @@ public abstract class CategorisedStatefulMonitor<S extends State> extends Statef
         return (S) state;
     }
 
-    protected void persistState(Class<? extends mt.edu.um.cs.rv.monitors.Monitor> c, CategorisedEvent e, S s){
-        this.getMonitorPersistenceProvider().save(c.getTypeName(), e.categoriseEvent(), s);
+    @Override
+    protected void persistState(Class<? extends mt.edu.um.cs.rv.monitors.Monitor> c, Event e, S s){
+
+        CategorisedEvent ce = null;
+        if (e instanceof CategorisedEvent){
+            ce = (CategorisedEvent) e;
+        }
+        else{
+            LOGGER.error("Unable to save state for monitor {}, as supplied event [{}] is not an instance of CategorisedEvent", c.getName(), e);
+        }
+
+        this.getMonitorPersistenceProvider().save(c.getTypeName(), ce.categoriseEvent(), s);
     }
 }
