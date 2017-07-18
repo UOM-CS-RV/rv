31ddd02a1ee5025a1974fc73fc4568ba70b32d2a
Edward Mallia <edmallia@gmail.com>
2017-07-18 11:07:06 +0200

    Adds error handlin gin the MonitorInvocationServiceActivator

--- event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/monitors/MonitorInvocationServiceActivator.java
+++ event-manager/src/main/java/mt/edu/um/cs/rv/eventmanager/monitors/MonitorInvocationServiceActivator.java
@@ -3,13 +3,18 @@ package mt.edu.um.cs.rv.eventmanager.monitors;
 import mt.edu.um.cs.rv.events.Event;
 import mt.edu.um.cs.rv.monitors.Monitor;
 import mt.edu.um.cs.rv.monitors.results.MonitorResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+
+import java.io.Serializable;
 
 /**
  * Created by dwardu on 19/01/2016.
  */
 public class MonitorInvocationServiceActivator
 {
+    private static Logger LOGGER = LoggerFactory.getLogger(MonitorInvocationServiceActivator.class);
 
     private Monitor monitor;
 
@@ -23,6 +28,15 @@ public class MonitorInvocationServiceActivator
     
     public MonitorResult invokeMonitor(Event e)
     {
-        return monitor.handleEvent(e, null);
+        try {
+            return monitor.handleEvent(e, null);
+        }
+        catch (Throwable throwable){
+            String msg = String.format("Unexpected Throwable occurred of type - [%s]", throwable.getClass());
+            LOGGER.error("{}. Creating and returning a FAILURE MonitorResult.", msg, throwable);
+            MonitorResult<Serializable> failure = MonitorResult.failure(null, throwable);
+            LOGGER.error("Created FAILURE MonitorResult [{}]", failure);
+            return failure;
+        }
     }
 }
